f9b6e048247b3cf45da316a96883ae54aea5d0a4
Daravuth24 <chaukoldaravuth21@kit.edu.kh>
2023-07-14 16:07:38 +0700

    Chat Application without database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomModule } from './room/room.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [RoomModule],
+  imports: [RoomModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
